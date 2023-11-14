1dbbf56b90eaa6cbd7ee0ee0624461cb9cec5b43
mahyarnafisi <mahyar.nafisi@gmail.com>
2023-11-14 13:20:23 +0100

    develop functionality of login form add error handling style error appear error message on screen

--- src/Components/Layouts/Main/ChartContainer/BarChart/BarChart.tsx
+++ src/Components/Layouts/Main/ChartContainer/BarChart/BarChart.tsx
@@ -1,54 +1,35 @@
-import React from 'react'
-import Styles from './BarChart.module.css'
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Tooltip,
-  Legend
-} from "chart.js";
+import React from "react";
+import Styles from "./BarChart.module.css";
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Tooltip, Legend } from "chart.js";
 import { Bar } from "react-chartjs-2";
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Tooltip,
-  Legend
-
-)
-
+ChartJS.register(CategoryScale, LinearScale, BarElement, Tooltip, Legend);
 
 function BarChart() {
-
-  const data={
+  const data = {
     labels: ["Q1", "Q2", "Q3", "Q4", "Q5", "Q6", "Q7", "Q8"],
     datasets: [
-     {
-      label:"Malmo",
-      data: [0.5, 0.67, 0.89, 0.37, 0.83, 0.53, 0.72, 0.55],
-      backgroundColor:'rgb(255, 99, 132)',
-      borderColor:"black",
-     
-     },
-     {
-      label:"Lund",
-      data: [0.53, 0.72, 0.5, 0.67, 0.5, 0.67, 0.89, 0.37],
-      backgroundColor:'rgb(54, 162, 235)',
-      borderColor:"black",
-      
-     },
-     {
-      label:"Eslov",
-      data: [0.37, 0.83, 0.53, 0.72, 0.5, 0.67, 0.89, 0.9],
-      backgroundColor:'rgb(75, 192, 192)',
-      borderColor:"black",
-    
-     }
+      {
+        label: "Malmo",
+        data: [0.5, 0.67, 0.89, 0.37, 0.83, 0.53, 0.72, 0.55],
+        backgroundColor: "rgb(255, 99, 132)",
+        borderColor: "black",
+      },
+      {
+        label: "Lund",
+        data: [0.53, 0.72, 0.5, 0.67, 0.5, 0.67, 0.89, 0.37],
+        backgroundColor: "rgb(54, 162, 235)",
+        borderColor: "black",
+      },
+      {
+        label: "Eslov",
+        data: [0.37, 0.83, 0.53, 0.72, 0.5, 0.67, 0.89, 0.9],
+        backgroundColor: "rgb(75, 192, 192)",
+        borderColor: "black",
+      },
     ],
-  }
+  };
 
-  const options={
+  const options = {
     // scales: {
     //   y: {
     //     beginAtZero: true,
@@ -65,15 +46,16 @@ function BarChart() {
     //     }
     //   }
     // }
+  };
 
-  }
-
-  return <div className={Styles.barChart}>
-    <div className={Styles.title}>
+  return (
+    <div className={Styles.barChart}>
+      <div className={Styles.title}>
         <h2>Total of Quality</h2>
+      </div>
+      <Bar className={Styles.chart} data={data} options={options} />
     </div>
-    <Bar className={Styles.chart} data={data} options={options}/>
-    </div>;
+  );
 }
 
-export default BarChart
+export default BarChart;
--- src/Components/Pages/Login/Login.tsx
+++ src/Components/Pages/Login/Login.tsx
@@ -2,8 +2,40 @@ import Styles from "./Login.module.css";
 import hestiaLogo from "./../../../assets/Image/hestiaAgora-logo.png";
 import { PasswordRoundedIcon, HelpOutlineRoundedIcon, LoginRoundedIcon } from "./../../UI/Icons/IconsLibrary";
 import React from "react";
+import { useForm } from "react-hook-form";
+
+/**Type Declaration */
+type formValues = {
+  username: string;
+  password: string;
+  message: string;
+  id: string | undefined;
+};
+
+/**Form Validation data pattern-values-messages */
+const formValidationData = {
+  username: {
+    value: true,
+    emailReGexPattern: /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/,
+    emailRequireMessage: "Invalid Username, please try again",
+  },
+  password: {
+    value: true,
+    passwordRequireMessage: "Invalid password, please try again",
+  },
+};
 
 function Login() {
+  const loginForm = useForm<formValues>(); //use useFrom from "React Hook Form"
+  const { register, handleSubmit, formState } = loginForm; //destructure register & handle submit & formState from loginForm variable
+
+  const { errors } = formState;
+
+  /**Submit form handler function */
+  const onSubmitHandler = (data: formValues) => {
+    console.log(data);
+  };
+
   return (
     <div className={Styles.login}>
       <section className={Styles.loginPanel}>
@@ -31,22 +63,44 @@ function Login() {
           </div>
         </div>
 
-        <form className={Styles.form}>
+        <form className={Styles.form} onSubmit={handleSubmit(onSubmitHandler)} noValidate>
           <div className={Styles.formRow}>
             <PasswordRoundedIcon />
           </div>
           <div className={Styles.formRow}>
-            <h2>Login with your username and passwrod:</h2>
+            <h2>Login with your username and password:</h2>
           </div>
 
           <div className={Styles.formRow}>
             <label htmlFor="username">Username:</label>
-            <input type="text" id="username" />
+            <input
+              placeholder="Please enter you email address"
+              type="text"
+              id="username"
+              {...register("username", {
+                required: {
+                  value: formValidationData.username.value,
+                  message: formValidationData.username.emailRequireMessage,
+                },
+              })}
+            />
+            <p className={Styles.inputControlErrorTextMessage}>{errors.username?.message} &nbsp;</p>
           </div>
 
           <div className={Styles.formRow}>
             <label htmlFor="password">Password:</label>
-            <input type="password" id="password" />
+            <input
+              placeholder="Please enter your password"
+              type="password"
+              id="password"
+              {...register("password", {
+                required: {
+                  value: formValidationData.password.value,
+                  message: formValidationData.password.passwordRequireMessage,
+                },
+              })}
+            />
+            <p className={Styles.inputControlErrorTextMessage}>{errors.password?.message} &nbsp;</p>
           </div>
 
           <div className={Styles.formRow}>
