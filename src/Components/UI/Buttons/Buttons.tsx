bab431125538751f80e24eb583938fa4b955ec2f
Roudabeh Adnani <rodabeh.adnani@gmail.com>
2023-11-16 10:00:51 +0100

    fix Contact Support

--- src/Components/UI/Buttons/Buttons.tsx
+++ src/Components/UI/Buttons/Buttons.tsx
@@ -4,10 +4,11 @@ import React from "react";
 interface buttonProps {
   children: React.ReactNode;
   type?: string;
+  onClick:React.MouseEventHandler<HTMLButtonElement> | undefined; 
 }
 
-function Button({ children, type }: buttonProps) {
-  return <button className={`${Styles.button} ${Styles[`${type}`]}`}>{children}</button>;
+function Button({ children, type, onClick }: buttonProps) {
+  return <button className={`${Styles.button} ${Styles[`${type}`]}`} onClick={onClick} >{children}</button>;
 }
 
 export default Button;
