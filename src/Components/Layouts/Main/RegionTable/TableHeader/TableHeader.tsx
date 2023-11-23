d2e08cab7493cdc5dbfadee5f0520c92e00cb8bf
Roudabeh Adnani <rodabeh.adnani@gmail.com>
2023-11-23 14:50:40 +0100

    fix TableHeader & TableItem

--- src/Components/Layouts/Main/RegionTable/TableHeader/TableHeader.tsx
+++ src/Components/Layouts/Main/RegionTable/TableHeader/TableHeader.tsx
@@ -3,13 +3,23 @@ import React from "react";
 
 function TableHeader() {
   return (
-    <div className={Styles.tableHeader}>
-      <h2 className={Styles.header__title}>Municipality List</h2>
-      <h3 className={Styles.header__country}>Country</h3>
-      <h3 className={Styles.header__region}>Region</h3>
-      <h3 className={Styles.header__municipality}>Municipality</h3>
-      <h3 className={Styles.header__qofBar}>Quality of Life</h3>
-      <h3 className={Styles.header__remove}>Remove</h3>
+    <div>
+      <div>
+        <h2 className={Styles.header__title}>Municipality List</h2>
+      </div>
+      <hr/>
+      <div className={Styles.tableHeader}>
+        {/* <h2 className={Styles.header__title}>Municipality List</h2> */}
+        <h3 className={Styles.header__country}>Country</h3>
+        <h3 className={Styles.header__region}>Region</h3>
+        <h3 className={Styles.header__municipality}>Municipality</h3>
+        <h3 className={Styles.header__qofBar}>Quality of Life</h3>
+        <h3 className={Styles.header__ageGroup}>Age Group</h3>
+        <h3 className={Styles.header__gender}>Gender</h3>
+        <h3 className={Styles.header__livingSituation}>Living Situation</h3>
+        <h3 className={Styles.header__assistance}>Assistance</h3>
+        <h3 className={Styles.header__remove}>Remove</h3>
+      </div>
     </div>
   );
 }
