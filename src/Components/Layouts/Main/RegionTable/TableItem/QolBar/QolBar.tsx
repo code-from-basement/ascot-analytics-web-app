50ffdade7d2c75c013b5a94e66427e48f5ff5fb8
Roudabeh Adnani <rodabeh.adnani@gmail.com>
2023-11-30 13:47:09 +0100

    fix table items

--- src/Components/Layouts/Main/RegionTable/TableItem/QolBar/QolBar.tsx
+++ src/Components/Layouts/Main/RegionTable/TableItem/QolBar/QolBar.tsx
@@ -1,11 +1,13 @@
 import Styles from "./QolBar.module.css";
 import React from "react";
 
-function QolBar() {
+function QolBar({qolAvg}:any) {
+  const calcFilterBarLength = qolAvg* 100;
+  console.log(calcFilterBarLength)
   return (
     <div className={Styles.bar}>
-      <div className={Styles.bar__filler}>&nbsp;</div>
-      <h5 className={Styles.bar__percentage}>50%</h5>
+      <div className={Styles.bar__filler}  style={{width:`${calcFilterBarLength}%`}}>&nbsp;</div>
+      <h5 className={Styles.bar__percentage}>{qolAvg}</h5>
     </div>
   );
 }
--- src/Components/Layouts/Main/RegionTable/TableList/TableList.tsx
+++ src/Components/Layouts/Main/RegionTable/TableList/TableList.tsx
@@ -7,7 +7,20 @@ function TableList() {
   const { listItem, limitationListItemError } = useGlobalContext();
   return (
     <div className={Styles.tableList}>
-      {listItem && listItem.map((item) => <TableItem key={item.id} item={item} id={item.id} />)}
+      {listItem && listItem.map((item) => 
+        <TableItem key={item.id} 
+          item={item} 
+          id={item.id} 
+          country={item.data.info.country}
+          region={item.data.info.region}
+          municipality={item.data.info.municipality}
+          ageGroup={item.data.info.age}
+          gender={item.data.info.gender}
+          livingSituation={item.data.info.livingSituation}
+          assistance={item.data.info.assistance}
+          qolAvg={item.data.qolAvg}
+           />
+      )}
       {limitationListItemError && <h1>error</h1>}
     </div>
   );
