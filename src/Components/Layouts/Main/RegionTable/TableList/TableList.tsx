eedeafaa33de386a88bf2715f313f029c7f803e7
Roudabeh Adnani <rodabeh.adnani@gmail.com>
2023-12-05 12:09:49 +0100

    ...

--- src/Components/Layouts/Main/RegionTable/TableList/TableList.tsx
+++ src/Components/Layouts/Main/RegionTable/TableList/TableList.tsx
@@ -12,11 +12,11 @@ function TableList() {
         <TableItem key={item.id} 
           item={item} 
           id={item.id} 
-          country={item.data.info.country}
-          region={item.data.info.region}
-          municipality={item.data.info.municipality}
-          ageGroup={item.data.info.age}
-          gender={item.data.info.gender}
+          country={item.data.info.country.charAt(0).toUpperCase() + item.data.info.country.replaceAll("_", " ").slice(1)}
+          region={item.data.info.region.charAt(0).toUpperCase() + item.data.info.region.replaceAll("_", " ").slice(1)}
+          municipality={item.data.info.municipality.charAt(0).toUpperCase() + item.data.info.municipality.replaceAll("_", " ").slice(1)}
+          ageGroup={item.data.info.ageGroup}
+          gender={item.data.info.gender.charAt(0).toUpperCase() + item.data.info.gender.replaceAll("_", " ").slice(1)}
           livingSituation={item.data.info.livingSituation.charAt(0).toUpperCase() + item.data.info.livingSituation.replaceAll("_", " ").slice(1)}
           assistance={item.data.info.assistance.charAt(0).toUpperCase() + item.data.info.assistance.replaceAll("_", " ").slice(1)}
           qolAvg={item.data.qolAvg}
