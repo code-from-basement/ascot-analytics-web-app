bd1064dadb32d52cd2620224d1411616b55cbd2a
mahyarnafisi <mahyar.nafisi@gmail.com>
2023-12-13 11:51:26 +0100

    implement the questions list button in bar chart component

--- src/Components/Layouts/Main/ChartContainer/BarChart/BarChart.tsx
+++ src/Components/Layouts/Main/ChartContainer/BarChart/BarChart.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
 import Styles from "./BarChart.module.css";
 import Chart from "react-apexcharts";
-import { ApexOptions } from "apexcharts";
+
 import { useGlobalContext } from "../../../../../Context/GlobalContext";
+import { HelpOutlineRoundedIcon } from "../../../../UI/Icons/IconsLibrary";
 
 function BarChart() {
   const { barChartData, isLoading }: any = useGlobalContext();
@@ -47,65 +48,13 @@ function BarChart() {
     // }
   };
 
-  // const [state, setState] = useState({
-  // options: {
-  // states: {
-  // normal: {
-  //   filter: {
-  //     type: "none",
-  //     value: 0,
-  //   },
-  // },
-  // hover: {
-  //   filter: {
-  //     type: "none",
-  //     value: 0.01,
-  //   },
-  // },
-  // active: {
-  //   allowMultipleDataPointsSelection: false,
-  //   filter: {
-  //     type: "none",
-  //     value: 0.15,
-  //   },
-  // },
-  // },
-  // dataLabels: {
-  //   enabled: false,
-  // },
-  // fill: {
-  //   type: "gradient",
-  //   gradient: {
-  //     shade: "light",
-  //     type: "vertical",
-  //     shadeIntensity: 0.25,
-  //     gradientToColors: undefined,
-  //     inverseColors: true,
-  //     opacityFrom: 0.8,
-  //     opacityTo: 1,
-  //     stops: [0, 70, 100],
-  //   },
-  // },
-  // yaxis: {
-  //   title: {
-  //     text: "Quality of Life",
-  //     offsetX: -5,
-  //     style: {
-  // color: "#222224",
-  //       fontSize: "1.4rem",
-  //       fontWeight: 600,
-  //     },
-  //   },
-  //   tooltip: {
-  //     enabled: false,
-  //     offsetX: 0,
-  //   },
-  //   min: 0,
-  //   max: 1,
-  // },
-  // },
-  // });
-
-  return <div className={Styles.barChart}>{!isLoading && <Chart options={barChartOptions} series={barChartData} type="bar" width="100%" height={320} />}</div>;
+  return (
+    <div className={Styles.barChart}>
+      <button className={Styles.questionLists}>
+        <HelpOutlineRoundedIcon />
+      </button>
+      {!isLoading && <Chart options={barChartOptions} series={barChartData} type="bar" width="100%" height={320} />}
+    </div>
+  );
 }
 export default BarChart;
